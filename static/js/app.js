e3efc106d34350f9ccff64081c1c5dd9edaaf71f
brentfindley@gmail.com <brentfindley@gmail.com>
2019-02-21 23:20:03 -0600

    initial commit

---
+++ static/js/app.js
@@ -0,0 +1,25 @@
+// from data.js
+var tableData = data;
+
+var button = d3.select("#filter-btn");
+
+button.on("click", function(){
+
+    d3.event.preventDefault();
+
+    var query = d3.select("#datetime").property("value");
+    console.log(query);
+    var filtered_results = tableData.filter(d => d.datetime === query)
+    console.log(filtered_results);
+
+    var tbody = d3.select("tbody");
+
+    filtered_results.forEach(function(entry) {
+        var row = tbody.append("tr")
+        Object.entries(entry).forEach(function([key,value]) {
+          row.append("td").text(value);
+        });
+    });
+    
+
+});
